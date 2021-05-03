a24772b05f5848bccc186a0fbace06bf785e0f16
Han Han Han <wooyeophan@gmail.com>
2021-05-03 23:10:37 +0900

    first commit

---
+++ Block.js
@@ -0,0 +1,30 @@
+export class Block {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 35;
+        this.padding = 2;
+
+        this.fill = false;
+        this.color = "#ff0000"
+    }
+
+    setBlockData(fill, color) {
+        this.fill = fill;
+        this.color = color;
+    }
+
+    update(delta) {
+
+    }
+
+    render(ctx) {
+        ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+        ctx.strokeRect(this.x * this.size + this.padding, this.y * this.size + this.padding, this.size - 1, this.size - 1);
+
+        if (this.fill) {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x * this.size + this.padding, this.y * this.size + this.padding, this.size - 1, this.size - 1);
+        }
+    }
+}
---
+++ Player.js
@@ -0,0 +1,20 @@
+export class Player {
+    constructor() {
+        this.blockSet = [
+            [
+                [{ x: -1, y: 0 }, { x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }],
+                [{ x: 0, y: 1 }, { x: 0, y: 0 }, { x: 0, y: -1 }, { x: 0, y: -2 }] // 과제
+            ], // 작대기
+            [], // J
+            [], // L
+            [], // S
+            [], // T
+            [],
+            []
+        ]
+    }
+
+    getNextBlockSet() {
+
+    }
+}
